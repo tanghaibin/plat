c1e06015299cda0b5a54aa51bd5dd91124352b6d
tanghaibin <tanghaibinhaohan@163.com>
2018-05-02 13:05:32 +0800

    fix

--- app/src/main/java/com/plat/app/config/JPAConfig.java
+++ app/src/main/java/com/plat/app/config/JPAConfig.java
@@ -2,8 +2,6 @@ package com.plat.app.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -24,7 +22,6 @@ import java.util.Properties;
  * @create 2018-04-27 下午5:12
  **/
 @Configuration
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @EnableTransactionManagement(proxyTargetClass = true)
 @EnableJpaRepositories(
         basePackages = {"com.plat.repository"},
